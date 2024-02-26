432c7d217a8730f455aa90c0713b2a8371e947d0
Matthew Carroll <matthewCmatt@gmail.com>
2024-02-25 23:10:50 -0600

    docs: comments for Simulator

--- src/graphics/simulator.ts
+++ src/graphics/simulator.ts
@@ -10,50 +10,52 @@ import {
 } from 'three';
 
 export default class Simulator {
-  private scene: Scene;
   private webgl: WebGLRenderer;
+  private scene: Scene;
   private camera: PerspectiveCamera;
   private controls: OrbitControls;
 
   private cube: Mesh;
 
   constructor() {
+    // Set up renderer
+    this.webgl = new WebGLRenderer();
+    this.webgl.setSize(window.innerWidth, window.innerHeight); // Default to full size of the window.
+    this.webgl.setAnimationLoop(() => {this.rerender()}); // Rerender the scene on every frame
+        
+    // Set up scene
     this.scene = new Scene();
     this.scene.background = new Color(0x303030);
-    this.webgl = new WebGLRenderer();
+
+    // Add a placeholder cube to the scene
+    const geometry = new BoxGeometry(1, 1, 1);
+    const material = new MeshBasicMaterial({ color: 0x00ffff });
+    this.cube = new Mesh(geometry, material);
+    this.scene.add(this.cube);
+
+    // Set up camera
     this.camera = new PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
       0.1,
       1000,
     );
-    
-    this.controls = new OrbitControls(this.camera, this.webgl.domElement);
-
     this.camera.position.z = 5;
-    this.controls.update();
-    const geometry = new BoxGeometry(1, 1, 1);
-    const material = new MeshBasicMaterial({ color: 0x00ff00 });
-    this.cube = new Mesh(geometry, material);
-    this.scene.add(this.cube);
 
-    // Default to full size of the window.
-    this.webgl.setSize(window.innerWidth, window.innerHeight);
-
-    // Rerender the scene on every frame
-    this.webgl.setAnimationLoop(() => {this.rerender()});
+    // Add orbit controls for the mouse
+    this.controls = new OrbitControls(this.camera, this.webgl.domElement);
+    this.controls.autoRotate = true;
   }
 
   public update() {
     // Update views based on controls (mouse)
     this.controls.update();
-    // Animate cube
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
   }
 
   public rerender() {
+    // Update the scene
     this.update();
+    // Render the frame
     this.webgl.render(this.scene, this.camera);
   }
 
