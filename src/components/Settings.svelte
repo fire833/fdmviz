74bc4b1a69e768b22009d48df7f6f8948ddb689d
Matthew Carroll <matthewCmatt@gmail.com>
2024-02-26 20:49:19 -0600

    feat: flesh out UI a bit more

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -1,32 +1,44 @@
 <div class="container">
   <h1>Settings</h1>
-  <div>
-    <button
-      on:click={() => {
-        alert('Changing mode ;)');
-      }}
-    >
-      Mode A
-    </button>
-    <button
-      on:click={() => {
-        alert('Changing mode ;)');
-      }}
-    >
-      Mode B
-    </button>
-    <button
-      on:click={() => {
-        alert('Changing mode ;)');
-      }}
-    >
-      Mode C
-    </button>
-  </div>
-  <div class="align">
-    Speed
-    <input type="range" />
-  </div>
+  <fieldset>
+    <legend>View Mode</legend>
+
+    <div class="left">
+      <input type="radio" id="huey" name="drone" value="huey" checked />
+      <label for="huey">Raw 3D File</label>
+    </div>
+
+    <div class="left">
+      <input type="radio" id="dewey" name="drone" value="dewey" />
+      <label for="dewey">Particle Sim</label>
+    </div>
+
+    <div class="left">
+      <input type="radio" id="louie" name="drone" value="louie" />
+      <label for="louie">Normal Map</label>
+    </div>
+  </fieldset>
+
+  <fieldset>
+    <legend>Features</legend>
+
+    <div class="align">
+      Speed
+      <input type="range" />
+    </div>
+    <div class="left">
+      <input type="checkbox" id="melty" />
+      <label for="melty">Melty Particles</label>
+    </div>
+    <div class="left">
+      <input type="checkbox" id="shaky" />
+      <label for="shaky">Shaky Bed</label>
+    </div>
+    <div class="left">
+      <input type="checkbox" id="transfer" />
+      <label for="transfer">Thermal Transfer</label>
+    </div>
+  </fieldset>
 </div>
 
 <style>
@@ -49,5 +61,26 @@
     justify-content: center;
     align-content: center;
     gap: 1rem;
+  }
+
+  fieldset {
+    padding: 1rem;
+    gap: 0.5rem;
+    display: flex;
+    flex-direction: column;
+
+    border-color: #fff;
+    border-style: solid;
+    border-width: 1.5px;
+  }
+
+  .left {
+    display: flex;
+    justify-content: start;
+    gap: 0.5rem;
+  }
+
+  legend {
+    padding: 0 0.5rem;
   }
 </style>
