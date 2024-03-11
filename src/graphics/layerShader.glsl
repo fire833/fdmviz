5df47fc1647a5e92359e4c01143407e916a00a3e
Matthew Carroll <matthewCmatt@gmail.com>
2024-03-11 03:14:15 -0500

    feat: make shader layers depend on depth

--- src/graphics/layerShader.glsl
+++ src/graphics/layerShader.glsl
@@ -1,6 +1,6 @@
 void main() {
     // Normalize the height value to the range [0, 1]
-    float height = (gl_FragCoord.y / 50.0);
-    float normalizedHeight = mod(height, 0.2) + 0.5;
+    float height = (gl_FragCoord.y / gl_FragCoord.w) / 50.0;
+    float normalizedHeight = mod(height, 5.0);
     gl_FragColor = vec4(normalizedHeight, normalizedHeight, normalizedHeight, 1.0);
 }
