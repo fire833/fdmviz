2b572ca1af344b4af10282a7fb065db9b20af508
Matthew Carroll <matthewCmatt@gmail.com>
2024-03-29 10:26:33 -0500

    feat: GitHub repo button

--- src/components/UserInterface.svelte
+++ src/components/UserInterface.svelte
@@ -2,9 +2,11 @@
   import type { SvelteComponent } from 'svelte';
   import PickModel from './PickModel.svelte';
   import Settings from './Settings.svelte';
+  import GitHub from './GitHub.svelte';
 
   let modal: SvelteComponent;
 </script>
 
 <PickModel bind:this={modal}></PickModel>
 <Settings on:openModal={modal.showModal()}></Settings>
+<GitHub></GitHub>
